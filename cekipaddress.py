4884bcc9ca4e5b91c295dd7c7c998b22f97062e9
DetaAgus <71640888+ItzAnu@users.noreply.github.com>
2022-10-13 14:59:11 +0800

    Add files via upload

---
+++ cekipaddress.py
@@ -0,0 +1,16 @@
+from ipaddress import ip_address
+import socket
+import requests
+import pprint
+import json
+
+hostname = input('Cantumkan Website: ')
+ip_address = socket.gethostbyname(hostname)
+
+request_url = 'https://geolocation-db.com/jsonp/' + ip_address
+response = requests.get(request_url)
+geolocation = response.content.decode()
+geolocation = geolocation.split("(")[1].strip(")")
+geolocation = json.loads(geolocation)
+for k,v in geolocation.items():
+    pprint.pprint(str(k) + ' : ' + str(v))
